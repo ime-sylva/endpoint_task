41e253d3b635098cc343ef7fe2604c893e766113
Ime Sylvanus <timsylva2021@gmail.com>
2023-09-12 00:45:55 +0100

    modified app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import os
 app = Flask(__name__)
 
 # Define a route that takes two query parameters: name and track
-@app.route('/info', methods=['GET'])
+@app.route('/api', methods=['GET'])
 def get_info():
     # Get query parameters
-    name = request.args.get('name')
+    name = request.args.get('slack_name')
     track = request.args.get('track')
 
     # Get current day of the week and UTC time
@@ -16,8 +16,8 @@ def get_info():
     current_utc_time = datetime.datetime.utcnow()
 
     # Get GitHub URLs for the file being run and full source code
-    file_url = os.getenv('GITHUB_FILE_URL')  # Set this environment variable with the file's GitHub URL
-    source_code_url = os.getenv('GITHUB_SOURCE_CODE_URL')  # Set this environment variable with the full source code's GitHub URL
+    file_url = 'https://github.com/ime-sylva/endpoint_task/blob/main/app.py'  # Set this environment variable with the file's GitHub URL
+    source_code_url = 'https://github.com/ime-sylva/endpoint_task/tree/main'  # Set this environment variable with the full source code's GitHub URL
 
     # Validate UTC time (+/- 2 hours)
     utc_offset = current_utc_time - datetime.datetime.now()
@@ -26,16 +26,16 @@ def get_info():
 
     # Construct the response JSON
     response = {
-        "Slack name": ime_sylva,
-        "Current day of the week": current_day,
-        "Current UTC time": current_utc_time.strftime('%Y-%m-%dT%H:%M:%SZ'),
-        "Track": track,
-        "GitHub URL of the file being run": file_url,
-        "GitHub URL of the full source code": source_code_url,
-        "Status Code": "Success"
+        "slack_name": name,
+        "current_day": current_day,
+        "utc_time": current_utc_time.strftime('%Y-%m-%dT%H:%M:%SZ'),
+        "track": track,
+        "github_file_url": file_url,
+        "github_repo_url": source_code_url,
+        "status_code": 200
     }
 
     return jsonify(response), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=1)
